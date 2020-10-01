93444e86f1034cf32cdf636900deb3692a4d76c1
Stracker <rremilavergne@gmail.com>
2020-10-01 22:22:07 +0200

    Update for 15.00.01

--- dllmain.cpp
+++ dllmain.cpp
@@ -60,7 +60,7 @@ CreateHook(MH::Monster::ctor, ConstructMonster, void*, void* this_ptr, unsigned 
 
 __declspec(dllexport) extern bool Load()
 {
-	if (std::string(GameVersion) != "416251") {
+	if (std::string(GameVersion) != "419914") {
 		LOG(ERR) << "Health Notes : Wrong version";
 		return false;
 	}
